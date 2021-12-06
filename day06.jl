d26c1593f24cf6f7a26f2cb5333acbdefff092bd
Kari Silvennoinen <kari@kalifi.org>
2021-12-06 08:08:37 +0200

    day 6

--- day06.jl
+++ day06.jl
@@ -1,7 +1,8 @@
-state = open("lanternfish.txt") do f
+initial_state = open("lanternfish.txt") do f
     parse.(Int, split(readline(f), ","))
 end
 
+state = copy(initial_state)
 # println("Intial sate: ", join(state, ","))
 for i in 1:80
     global state = state .- 1
@@ -13,3 +14,16 @@ for i in 1:80
 end
 
 println(length(state))
+
+buckets = zeros(Int, 9)
+for s in initial_state
+    buckets[s+1] += 1
+end
+# println(join(buckets, ","))
+
+for i in 1:256
+    global buckets = circshift(buckets, -1)
+    buckets[7] += buckets[9]
+    # println(i,": ",join(buckets, ","), " ", sum(buckets))
+end
+println(sum(buckets))
