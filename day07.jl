7ec367aed8c2105ddd66eaa38ce924a3074af5bf
Kari Silvennoinen <kari@kalifi.org>
2021-12-07 09:19:11 +0200

    day 7

--- day07.jl
+++ day07.jl
@@ -2,11 +2,19 @@ depths = open("crabs.txt") do f
     parse.(Int, split(readline(f), ","))
 end
 
-# println(minimum(collect(i -> sum(map(x -> abs(x - i), depths)), collect(minimum(depths):maximum(depths)))))
-
 min_fuel = Inf
 for i in minimum(depths):maximum(depths)
     fuel = sum(map(x -> abs(x - i), depths))
+    if fuel < min_fuel
+        global min_fuel = fuel
+    end
+end
+
+println(min_fuel)
+
+min_fuel = Inf
+for i in minimum(depths):maximum(depths)
+    fuel = sum(map(x -> abs(x - i)*(1+abs(x - i)) ÷ 2, depths))
     if fuel < min_fuel
         global min_fuel = fuel
     end
