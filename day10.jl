26fbbeebc22d8bd475d581753171c8f0b3500db7
Kari Silvennoinen <kari@kalifi.org>
2021-12-10 10:12:08 +0200

    day 10

--- day10.jl
+++ day10.jl
@@ -2,25 +2,27 @@ nav = open("navigation.txt") do f
     readlines(f)
 end
 
-points = Dict(')' => 3, ']' => 57, '}' => 1197, '>' => 25137)
+syntax_points = Dict(')' => 3, ']' => 57, '}' => 1197, '>' => 25137)
+autocomplete_points = Dict(')' => 1, ']' => 2, '}' => 3, '>' => 4)
 opposite = Dict(')' => '(', ']' => '[', '}' => '{', '>' => '<')
+ropposite = Dict(v => k for (k,v) in opposite)
 
-score = 0
+syntax_score = 0
+autocomplete_scores = []
 for line in nav
-    global score
+    global syntax_score, autocomplete_score
+    syntax_error = false
     # println("new line")
     stack = Vector{Char}()
     for i in line[1:end]
-        if i in keys(points)
-            if isempty(stack)
-                # println("syntax error at 1, line starts with closing")
-                break
-            elseif stack[end] == opposite[i]
+        if i in keys(opposite)
+            if !isempty(stack) && stack[end] == opposite[i]
                 pop!(stack)
                 # println("closing $(opposite[i]) ", join(stack))
             else
                 # println("syntax error at $(i), expecting $(stack[end])")
-                score += points[i]
+                syntax_score += syntax_points[i]
+                syntax_error = true
                 break
             end
         else
@@ -28,8 +30,16 @@ for line in nav
             # println("adding $(i) to stack ", join(stack))
         end
     end
-    if !isempty(stack)
-        # println("line incomplete")
+    if !isempty(stack) && !syntax_error
+        # println("line incomplete ", map(x -> ropposite[x], reverse(join(stack))))
+        score = 0
+        for i in map(x -> ropposite[x], reverse(join(stack)))
+            score = score * 5 + autocomplete_points[i]
+        end
+        push!(autocomplete_scores, score)
     end
 end
-println(score)
+
+sort!(autocomplete_scores)
+println(syntax_score)
+println(autocomplete_scores[ceil(Int, length(autocomplete_scores)/2)])
