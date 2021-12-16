227adf965fd21e56d6c9e498cff5204f938e9e55
Kari Silvennoinen <kari@kalifi.org>
2021-12-16 19:27:31 +0200

    day 16

--- day16.jl
+++ day16.jl
@@ -7,13 +7,13 @@ struct Packet
     type_id::Int
     length::Int
     sub_packets::Vector{Packet}
-    value::Union{Nothing, Int}
+    value::Int
 end
 
 function parse_packet(packet, depth = 0)
     version = parse(Int, join(Int.(packet[1:3])), base = 2)
     type_id = parse(Int, join(Int.(packet[4:6])), base = 2)
-    println("$(repeat(' ', 2*depth))packet is version $(version) and type $(type_id)")
+    # println("$(repeat(' ', 2*depth))packet is version $(version) and type $(type_id)")
     packet_length = 6
     sub_packets = []
     if type_id == 4
@@ -27,35 +27,51 @@ function parse_packet(packet, depth = 0)
             end
         end
         value = parse(Int, binary_value, base = 2)
-        println("$(repeat(' ', 2*depth))packet contains value $(value) (length $(packet_length))")
+        # println("$(repeat(' ', 2*depth))packet contains value $(value) (length $(packet_length))")
     else
         # operator packet
-        value = nothing
         if packet[7] == 0 # length type id
             subpackets_length = parse(Int, join(Int.(packet[8:22])), base = 2)
             packet_length += 1 + 15 + subpackets_length
-            println("$(repeat(' ', 2*depth))packet has $(subpackets_length) bits of sub-packets (total length $(packet_length))")
+            # println("$(repeat(' ', 2*depth))packet has $(subpackets_length) bits of sub-packets (total length $(packet_length))")
             processed = 0
             while processed < subpackets_length
-                println("$(repeat(' ', 2*depth))parsing packet at range $(23+processed):$(23+subpackets_length-1)")
+                # println("$(repeat(' ', 2*depth))parsing packet at range $(23+processed):$(23+subpackets_length-1)")
                 push!(sub_packets, parse_packet(packet[23+processed:23+subpackets_length-1], depth+1))
                 processed += sub_packets[end].length
             end
         elseif packet[7] == 1
             sub_packet_count = parse(Int, join(Int.(packet[8:18])), base = 2)
-            println("$(repeat(' ', 2*depth))packet has $(sub_packet_count) sub-packets")
+            # println("$(repeat(' ', 2*depth))packet has $(sub_packet_count) sub-packets")
             ptr = 0
             while length(sub_packets) < sub_packet_count
-                println("$(repeat(' ', 2*depth))parsing packet at range $(19+ptr)")
+                # println("$(repeat(' ', 2*depth))parsing packet at range $(19+ptr)")
                 push!(sub_packets, parse_packet(packet[19+ptr:end], depth +1))
                 ptr += sub_packets[end].length
             end
             packet_length += ptr + 11 + 1
         else
-            throw("$(repeat(' ', 2*depth))unknown length type id $(packet[7])")
+            throw("unknown length type id $(packet[7])")
+        end
+        value = if type_id == 0 # sum packet
+            sum(map(x -> x.value, sub_packets))
+        elseif type_id == 1 # product packet
+            prod(map(x -> x.value, sub_packets))
+        elseif type_id == 2 # minimum packet
+            minimum(map(x -> x.value, sub_packets))
+        elseif type_id == 3 # maximum packet
+            maximum(map(x -> x.value, sub_packets))
+        elseif type_id == 5 # greater than 
+            sub_packets[1].value > sub_packets[2].value ? 1 : 0
+        elseif type_id == 6 # smaller than
+            sub_packets[1].value < sub_packets[2].value ? 1 : 0
+        elseif type_id == 7 # equal
+            sub_packets[1].value == sub_packets[2].value ? 1 : 0
+        else
+            throw("unknown packet type $(type_id)") 
         end
     end
-    println("$(repeat(' ', 2*depth))end of packet (length $(packet_length))")
+    # println("$(repeat(' ', 2*depth))end of packet (length $(packet_length))")
     return Packet(version, type_id, packet_length, sub_packets, value)
 end
 
@@ -84,3 +100,13 @@ end
 #println(version_sum(parse_packet(parse_hex("C0015000016115A2E0802F182340"))))
 #println(version_sum(parse_packet(parse_hex("A0016C880162017C3686B18A3D4780"))))
 println(version_sum(parse_packet(parse_hex(bits))))
+
+# println(parse_packet(parse_hex("C200B40A82")).value)
+# println(parse_packet(parse_hex("04005AC33890")).value)
+# println(parse_packet(parse_hex("880086C3E88112")).value)
+# println(parse_packet(parse_hex("CE00C43D881120")).value)
+# println(parse_packet(parse_hex("D8005AC2A8F0")).value)
+# println(parse_packet(parse_hex("F600BC2D8F")).value)
+# println(parse_packet(parse_hex("9C005AC2F8F0")).value)
+# println(parse_packet(parse_hex("9C0141080250320F1802104A08  ")).value)
+println(parse_packet(parse_hex(bits)).value)
